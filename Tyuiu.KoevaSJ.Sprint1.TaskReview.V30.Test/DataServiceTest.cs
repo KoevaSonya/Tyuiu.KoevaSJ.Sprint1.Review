2d96e7a1ae95fa0da94d96637272cbe8938fc6a8
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-10-04 11:27:38 +0500

    Add project files.

---
+++ Tyuiu.KoevaSJ.Sprint1.TaskReview.V30.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KoevaSJ.Sprint1.TaskReview.V30.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint1.TaskReview.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 5.472;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KoevaSJ.Sprint1.TaskReview.V30/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KoevaSJ.Sprint1.TaskReview.V30.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint1.TaskReview.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Коева С. Ю. | АСОиУБ-23-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                  *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                           *");
+            Console.WriteLine("* Задание #7                                                                 *");
+            Console.WriteLine("* Вариант #30                                                                *");
+            Console.WriteLine("* Выполнила: Коева Софья Юрьевена | АСОиУБ-23-1                              *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение             *");
+            Console.WriteLine("*  по исходным значениям данных, вводимых пользователем.                     *");
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+          
+
+            double x, y;
+
+            Console.WriteLine("Введите значение Х:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
